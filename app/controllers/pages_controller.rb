c7e9c271054d73f56af8a79ee12584977a91db19
v4v9a <v4v9a@ugrad.cs.ubc.ca>
2015-10-29 19:49:28 -0700

    Made all pages and rerouted

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+
+	def home
+	end
+	
+	def search
+	end
+	
+	def browse
+	end
+	
+	def login
+	end
+	
+	def admin
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root to: 'pages#home'
+  get '/search' => 'pages#search'
+  get '/browse' => 'pages#browse'
+  get '/login' => 'pages#login'
+  get '/admin' => 'pages#admin'
+  
+  get '*path' => 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
