3ac1447710c3f0e236209471bd0af5a5aeb86bad
David Zhang <dzhangd95@gmail.com>
2015-11-01 16:57:22 -0800

    Added creating and editing of schools

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,12 +1,38 @@
 class SchoolsController < ApplicationController
+	def index
+		@schools = School.all
+	end
+
+	def show
+		@school = School.find(params[:id])
+	end
+
 	def new
+		@school = School.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
 	end
 
 	def create
 		@school = School.new(school_params)
 
-		@school.save
-		redirect_to @school
+		if @school.save
+			redirect_to @school
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@school = School.find(params[:id])
+
+		if @school.update(school_params)
+			redirect_to @school
+		else
+			render 'edit'
+		end
 	end
 
 	private
