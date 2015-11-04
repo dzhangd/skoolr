23a3284befe9e5d7815401a3ef4faef06d3e4623
Vincent Wang <yixiwang@hotmail.com>
2015-11-04 04:30:32 -0800

    Finished school parser for CSV files only

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,12 @@
+require 'csv'
+
 class School < ActiveRecord::Base
+	def self.import(file)
+	::CSV.foreach(file.path, headers: true) do |row|
+		School.create! row.to_hash
+	end
+end
+
   validates :name, presence: true
   validates :address, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
 
-  resources :schools
+  resources :schools do 
+    collection {post :import}
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
