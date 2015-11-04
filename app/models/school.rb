77d4d86cd78df565bc06f541a42f62ae01192aaf
David Zhang <dzhangd95@gmail.com>
2015-11-03 22:01:55 -0800

    Started work on parsing xls files

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,12 @@
 class School < ActiveRecord::Base
   validates :name, presence: true
   validates :address, presence: true
+
+  def self.import(file)
+    xls = Roo::Spreadsheet.open(file)
+    xls.sheet.each(name: 'School Name', address: 'Address') do |hash|
+      School.create! hash
+    end
+  end
+
 end
