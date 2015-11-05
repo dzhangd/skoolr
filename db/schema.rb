4cf30468ee6d58e2c0343967c4ca3ae10867704a
Joseph Choi <robocup30@hotmail.com>
2015-11-05 13:34:31 -0800

    Renamed some grade columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103221525) do
+ActiveRecord::Schema.define(version: 20151105212846) do
 
   create_table "grades", force: :cascade do |t|
     t.string   "subject"
-    t.integer  "Grade"
-    t.integer  "AverageMark"
+    t.integer  "grade"
+    t.integer  "average_mark"
     t.integer  "school_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   add_index "grades", ["school_id"], name: "index_grades_on_school_id"
