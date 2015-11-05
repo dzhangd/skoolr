ad828af4708190ef334dd6e5c624323c8afd0d4b
David Zhang <dzhangd95@gmail.com>
2015-11-04 22:05:42 -0800

    Added city to addresses and fixed duplication check

--- app/models/school.rb
+++ app/models/school.rb
@@ -8,13 +8,13 @@ class School < ActiveRecord::Base
     xls = Roo::Spreadsheet.open(file)
     xls.each(name: 'SCHOOL_NAME', address: 'ADDRESS') do |hash|
       if valid?(hash)
-        School.create(name: hash[:name].split(' ')[0...-1].join(' '), address: hash[:address])
+        School.create(name: hash[:name].split(' ')[0...-1].join(' '), address: hash[:address] + " Vancouver")
       end
     end
   end
 
   private
   def self.valid?(hash)
-    return !School.exists?(name: hash[:name]) && hash[:name] != "SCHOOL_NAME" && hash[:name].split.last.delete('.') == "Sec"
+    return !School.exists?(name: hash[:name].split(' ')[0...-1].join(' ')) && hash[:name] != "SCHOOL_NAME" && hash[:name].split.last.delete('.') == "Sec"
   end
 end
