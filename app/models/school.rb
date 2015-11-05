ca2f3b978401406cc10ab1f78480ca0737e5ba82
Vincent Wang <yixiwang@hotmail.com>
2015-11-05 01:51:50 -0800

    Modified CSV uploader to update schools and have no duplicates

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,9 +1,17 @@
-require 'csv'
 
 class School < ActiveRecord::Base
+	require 'csv'
+
 	def self.import(file)
-	::CSV.foreach(file.path, headers: true) do |row|
-		School.create! row.to_hash
+	CSV.foreach(file.path, headers: true) do |row|
+		school_hash = row.to_hash
+		schools = School.where(name: school_hash["name"])
+
+		if schools.count == 1
+			schools.first.update_attributes(school_hash)
+			else
+		School.create!(school_hash)
+		end
 	end
 end
 
