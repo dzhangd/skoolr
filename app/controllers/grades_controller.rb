ed957757305a2a105ca36e00ecbe3c8ae4c9963a
Joseph Choi <robocup30@hotmail.com>
2015-11-11 23:17:24 -0800

    Initial implementation of grades

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,2 +1,12 @@
 class GradesController < ApplicationController
+  def create
+    @school = School.find(params[:school_id])
+    @grade = @school.grades.create(grade_params)
+    redirect_to school_path(@school)
+  end
+ 
+  private
+    def grade_params
+      params.require(:grade).permit(:subject, :grade, :average_mark)
+    end
 end
