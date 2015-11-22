9ceab0b832cbbe8162461810ffa1b939cdd30b1e
Joseph Choi <robocup30@hotmail.com>
2015-11-21 17:05:09 -0800

    Added survey to browse view

--- app/assets/javascripts/browse/browse.js
+++ app/assets/javascripts/browse/browse.js
@@ -8,16 +8,24 @@ app.controller(
 			$scope.searchResults = $scope.SCHOOLS;
 			$scope.searchQuery = "";
 			$scope.GRADES = [];
+			$scope.SURVEYS = [];
 			//functions
 			$scope.loadSchoolInfo = function(id) {
 				$scope.schoolId = $scope.SCHOOLS[id].index_id;
 				$scope.schoolName = $scope.SCHOOLS[id].name;
 				$scope.schoolAddress = $scope.SCHOOLS[id].address;
 				$scope.schoolGrades = [];
+				$scope.schoolSurveys = [];
 				var len = $scope.GRADES.length;
 				for (var k = 0; k < len; k++) {
 					if($scope.GRADES[k].school_id == $scope.schoolId) {
 						$scope.schoolGrades.push($scope.GRADES[k]);
+					}
+				}
+				var len = $scope.SURVEYS.length;
+				for (var k = 0; k < len; k++) {
+					if($scope.SURVEYS[k].school_id == $scope.schoolId) {
+						$scope.schoolSurveys.push($scope.SURVEYS[k]);
 					}
 				}
 				
