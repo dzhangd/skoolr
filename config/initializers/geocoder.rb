06d2a7d0c1d06e8893f01b07164999746f0fc941
Joseph Choi <robocup30@hotmail.com>
2015-11-27 01:51:26 -0800

    Increased geocoder timeout

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,7 +1,7 @@
 Geocoder.configure(
 
   :http_proxy => ENV['http://quotaguard4235:086b66185f1e@proxy.quotaguard.com:9292'],
-  :timeout => 5,
+  :timeout => 20,
   :lookup => :google,
   :units => :km,
   :distances => :spherical
